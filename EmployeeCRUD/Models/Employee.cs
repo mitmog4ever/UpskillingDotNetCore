efdc5d5cf6539196a918e10e0cc13ae11778de38
akram <farratakram@gmail.com>
2019-12-30 01:43:18 +0200

    first commit

---
+++ EmployeeCRUD/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCRUD.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+        [Required]
+        public string CIN { get; set; }
+        [Required]
+        public string Sexe { get; set; }
+        [Required]
+        public string Departement { get; set; }
+        [Required]
+        public float Salaire{ get; set; }
+
+    }
+}
